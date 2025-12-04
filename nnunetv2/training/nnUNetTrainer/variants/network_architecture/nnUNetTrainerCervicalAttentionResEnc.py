fe97eee543c0e9166c91019b29fb9b499082322f
Brandon Kim <anomaliscript@gmail.com>
2025-12-03 23:20:56 -0500

    batch size override

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
@@ -69,6 +69,9 @@ class nnUNetTrainerCervicalAttentionResEnc(nnUNetTrainer):
 
             print(f"   Patch size: {original_patch_size} -> {reduced_patch_size}")
 
+        # well well well you can also edit batch size here too
+        self.configuration_manager.configuration['batch_size'] = 20
+
         # Call parent initialization
         super().initialize()
 
