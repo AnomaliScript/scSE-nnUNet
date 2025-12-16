9bf82fa908589d230b0b2acfe5b914ad49b6cd04
Brandon Kim <anomaliscript@gmail.com>
2025-12-15 19:51:30 -0500

    64^3_64bsize

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
@@ -59,16 +59,16 @@ class nnUNetTrainerCervicalAttentionResEnc(nnUNetTrainer):
             # edited_patch_size = [32, 32, 32]
 
             # OPTION 2: Hard-set to 64³
-            # edited_patch_size = [64, 64, 64]
+            edited_patch_size = [64, 64, 64]
 
             # #OPTION 3: Adjusted Anisotropic (follows spine anatomy)
-            edited_patch_size = [32, 32, 64]
+            # edited_patch_size = [32, 32, 64]
 
             self.configuration_manager.configuration['patch_size'] = edited_patch_size
             print(f"   Patch size: {original_patch_size} -> {edited_patch_size}")
 
         # well well well you can also edit batch size here too
-        self.configuration_manager.configuration['batch_size'] = 128
+        self.configuration_manager.configuration['batch_size'] = 64
 
         # Call parent initialization
         super().initialize()
