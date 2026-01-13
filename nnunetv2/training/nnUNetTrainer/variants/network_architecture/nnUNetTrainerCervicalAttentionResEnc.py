c2165e7857ec1b7ac7f84cf5c2e541d7f1e08cca
Brandon Kim <anomaliscript@gmail.com>
2026-01-13 17:45:15 -0500

    172x172x172

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
@@ -73,7 +73,7 @@ class nnUNetTrainerCervicalAttentionResEnc(nnUNetTrainer):
             original_patch_size = self.configuration_manager.configuration['patch_size']
 
             # OPTION 1: Largest 128 x 128 x 256
-            edited_patch_size = [164, 164, 164]
+            edited_patch_size = [172, 172, 172]
 
             # OPTION 2: Hard-set to 96³
             # edited_patch_size = [96, 96, 96]
@@ -85,7 +85,7 @@ class nnUNetTrainerCervicalAttentionResEnc(nnUNetTrainer):
             print(f"   Patch size: {original_patch_size} -> {edited_patch_size}")
 
         # well well well you can also edit batch size here too
-        self.configuration_manager.configuration['batch_size'] = 12
+        self.configuration_manager.configuration['batch_size'] = 6
 
         # Call parent initialization
         super().initialize()
