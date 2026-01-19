9c09fa5eca6df6e6cc0e387ee0906aa96eb98388
Brandon Kim <anomaliscript@gmail.com>
2026-01-19 13:32:25 -0500

    hopefully batchsize 7 i reasonable (in terms of sec/epoch)

--- nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
+++ nnunetv2/training/nnUNetTrainer/variants/network_architecture/nnUNetTrainerCervicalAttentionResEnc.py
@@ -78,7 +78,7 @@ class nnUNetTrainerCervicalAttentionResEnc(nnUNetTrainer):
             print(f"   Patch size: {original_patch_size} -> {edited_patch_size}")
 
         # well well well you can also edit batch size here too
-        self.configuration_manager.configuration['batch_size'] = 10
+        self.configuration_manager.configuration['batch_size'] = 7
 
         # Call parent initialization
         super().initialize()
